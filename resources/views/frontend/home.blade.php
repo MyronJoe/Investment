3cddff1b3c12e2c7d18648b449c64d7f3ee40c31
Myron <josephonyekachi77@gmail.com>
2023-04-19 21:11:46 -0800

    updated

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -55,75 +55,7 @@
 
 
     <!-- Feature Section Starts Here -->
-    <section class="feature-section padding-bottom overflow-hidden">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-6 col-md-10">
-                    <div class="section__header text-center">
-                        <h2 class="section__header-title">Best for Hyip Investment</h2>
-                        <p>Aenean vulputate eleifend tellus. Aenean leo ligul porttitoeu consequat vitae eleifend </p>
-                    </div>
-                </div>
-            </div>
-            <div class="feature__slider">
-                <div class="single-slide">
-                    <div class="feature__item">
-                        <div class="feature__item-icon">
-                            <img src="assets/images/feature/1.png" alt="feature">
-                        </div>
-                        <div class="feature__item-content">
-                            <h4 class="title">Get More Profit</h4>
-                            <p>Maecenas tempuslusning eget condim entum and rhoncus eumpesit.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="single-slide">
-                    <div class="feature__item">
-                        <div class="feature__item-icon">
-                            <img src="assets/images/feature/2.png" alt="feature">
-                        </div>
-                        <div class="feature__item-content">
-                            <h4 class="title">24/7 Support</h4>
-                            <p>Maecenas tempuslusning eget condim entum and rhoncus eumpesit.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="single-slide">
-                    <div class="feature__item">
-                        <div class="feature__item-icon">
-                            <img src="assets/images/feature/3.png" alt="feature">
-                        </div>
-                        <div class="feature__item-content">
-                            <h4 class="title">Strong Protection</h4>
-                            <p>Maecenas tempuslusning eget condim entum and rhoncus eumpesit.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="single-slide">
-                    <div class="feature__item">
-                        <div class="feature__item-icon">
-                            <img src="assets/images/feature/4.png" alt="feature">
-                        </div>
-                        <div class="feature__item-content">
-                            <h4 class="title">Relability</h4>
-                            <p>Maecenas tempuslusning eget condim entum and rhoncus eumpesit.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="single-slide">
-                    <div class="feature__item">
-                        <div class="feature__item-icon">
-                            <img src="assets/images/feature/4.png" alt="feature">
-                        </div>
-                        <div class="feature__item-content">
-                            <h4 class="title">Relability</h4>
-                            <p>Maecenas tempuslusning eget condim entum and rhoncus eumpesit.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('frontend.include.feature')
     <!-- Feature Section Ends Here -->
 
 
