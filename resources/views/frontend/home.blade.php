c5d25075e8efd60e0f29d7d6809fdb6b44f6988f
Myron <josephonyekachi77@gmail.com>
2023-04-19 21:09:30 -0800

    updated

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -50,63 +50,7 @@
 
 
     <!-- Profit Calculation Section Starts Here -->
-    <section class="profit-calculation padding-bottom">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-6 d-none d-lg-block">
-                    <div class="section__thumb profit__calculation__thumb rtl me-5">
-                        <img src="assets/images/calculate-profit/thumb.png" alt="profit-calculation">
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="profit__calculation__content">
-                        <div class="section__header">
-                            <h2 class="section__header-title">Calculate Your Hyip investment</h2>
-                            <p>Aenean vulputate eleifend tellus. Aenean leo ligul porttitoeu consequat vitae eleifend acenim</p>
-                        </div>
-                        <form class="profit__calculation__form">
-                            <div class="row g-4">
-                                <div class="col-md-6 col-lg-12 col-xl-6 col-sm-6">
-                                    <div class="form-group">
-                                        <label class="form-label">Plan</label>
-                                        <select class="nice-select w-100 h-50">
-                                            <option>Select Plan</option>
-                                            <option>Silver Plan</option>
-                                            <option>Gold Plan</option>
-                                            <option>Platium Plan</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-lg-12 col-xl-6 col-sm-6">
-                                    <div class="form-group">
-                                        <label class="form-label">Compounding</label>
-                                        <select class="nice-select w-100 h-50">
-                                            <option>05%</option>
-                                            <option>15%</option>
-                                            <option>20%</option>
-                                            <option>25%</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-lg-12 col-xl-6 col-sm-6">
-                                    <div class="form-group">
-                                        <label class="form-label">Profit</label>
-                                        <input type="text" class="form-control form--control" placeholder="$60,000">
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-lg-12 col-xl-6 col-sm-6">
-                                    <div class="form-group">
-                                        <label class="form-label">Total Returns</label>
-                                        <input type="text" class="form-control form--control" placeholder="$800000">
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('frontend.include.calculate')
     <!-- Profit Calculation Section Ends Here -->
 
 
