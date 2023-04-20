bd463121995d005a1a31e62bfa8a8efa74d08aa0
Myron <josephonyekachi77@gmail.com>
2023-04-19 20:44:29 -0800

    updated

---
+++ resources/views/frontend/include/plans.blade.php
@@ -0,0 +1,198 @@
+<section class="plan-section padding-bottom">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-10">
+                <div class="section__header text-center">
+                    <h2 class="section__header-title">Our Investment Plan</h2>
+                    <p>Aenean vulputate eleifend tellus. Aenean leo ligul porttitoeu consequat vitae eleifend </p>
+                </div>
+            </div>
+        </div>
+        <div class="row g-4 justify-content-center">
+            <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6">
+                <div class="plan__item">
+                    <div class="plan__item-header">
+                        <h2 class="plan-parcent">5%</h2>
+                        <p class="plan-parcent-info">After 6 Hours</p>
+                    </div>
+                    <div class="plan__item-body">
+                        <ul class="plan__info">
+                            <li>
+                                <span class="title">Min :</span>
+                                <span class="value">15 USD</span>
+                            </li>
+                            <li>
+                                <span class="title">Max :</span>
+                                <span class="value">30 USD</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="plan__item-footer">
+                        <p class="footer-info"> FOREVER</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6">
+                <div class="plan__item">
+                    <div class="plan__item-header">
+                        <h2 class="plan-parcent">69%</h2>
+                        <p class="plan-parcent-info">Hourly</p>
+                    </div>
+                    <div class="plan__item-body">
+                        <ul class="plan__info">
+                            <li>
+                                <span class="title">Min :</span>
+                                <span class="value">15 USD</span>
+                            </li>
+                            <li>
+                                <span class="title">Max :</span>
+                                <span class="value">30 USD</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="plan__item-footer">
+                        <p class="footer-info"> FOR 8 HOURS</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6">
+                <div class="plan__item">
+                    <div class="plan__item-header">
+                        <h2 class="plan-parcent">120%</h2>
+                        <p class="plan-parcent-info">Hourly</p>
+                    </div>
+                    <div class="plan__item-body">
+                        <ul class="plan__info">
+                            <li>
+                                <span class="title">Min :</span>
+                                <span class="value">15 USD</span>
+                            </li>
+                            <li>
+                                <span class="title">Max :</span>
+                                <span class="value">30 USD</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="plan__item-footer">
+                        <p class="footer-info"> After 3 Months</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6">
+                <div class="plan__item">
+                    <div class="plan__item-header">
+                        <h2 class="plan-parcent">200%</h2>
+                        <p class="plan-parcent-info">After 6 Hours</p>
+                    </div>
+                    <div class="plan__item-body">
+                        <ul class="plan__info">
+                            <li>
+                                <span class="title">Min :</span>
+                                <span class="value">15 USD</span>
+                            </li>
+                            <li>
+                                <span class="title">Max :</span>
+                                <span class="value">30 USD</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="plan__item-footer">
+                        <p class="footer-info"> After 6 Months</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6">
+                <div class="plan__item">
+                    <div class="plan__item-header">
+                        <h2 class="plan-parcent">250%</h2>
+                        <p class="plan-parcent-info">After 8 Hours</p>
+                    </div>
+                    <div class="plan__item-body">
+                        <ul class="plan__info">
+                            <li>
+                                <span class="title">Min :</span>
+                                <span class="value">15 USD</span>
+                            </li>
+                            <li>
+                                <span class="title">Max :</span>
+                                <span class="value">30 USD</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="plan__item-footer">
+                        <p class="footer-info"> FOREVER</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6">
+                <div class="plan__item">
+                    <div class="plan__item-header">
+                        <h2 class="plan-parcent">270%</h2>
+                        <p class="plan-parcent-info">Hourly</p>
+                    </div>
+                    <div class="plan__item-body">
+                        <ul class="plan__info">
+                            <li>
+                                <span class="title">Min :</span>
+                                <span class="value">15 USD</span>
+                            </li>
+                            <li>
+                                <span class="title">Max :</span>
+                                <span class="value">30 USD</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="plan__item-footer">
+                        <p class="footer-info"> FOR 10 Months</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6">
+                <div class="plan__item">
+                    <div class="plan__item-header">
+                        <h2 class="plan-parcent">300%</h2>
+                        <p class="plan-parcent-info">Hourly</p>
+                    </div>
+                    <div class="plan__item-body">
+                        <ul class="plan__info">
+                            <li>
+                                <span class="title">Min :</span>
+                                <span class="value">15 USD</span>
+                            </li>
+                            <li>
+                                <span class="title">Max :</span>
+                                <span class="value">30 USD</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="plan__item-footer">
+                        <p class="footer-info"> After 1 Year</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6">
+                <div class="plan__item">
+                    <div class="plan__item-header">
+                        <h2 class="plan-parcent">320%</h2>
+                        <p class="plan-parcent-info">Hourly</p>
+                    </div>
+                    <div class="plan__item-body">
+                        <ul class="plan__info">
+                            <li>
+                                <span class="title">Min :</span>
+                                <span class="value">15 USD</span>
+                            </li>
+                            <li>
+                                <span class="title">Max :</span>
+                                <span class="value">30 USD</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="plan__item-footer">
+                        <p class="footer-info"> After 1.2 Years</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
