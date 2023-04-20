23dc2f422e5d3487f40ac6d2e08f64cfcd60511e
Myron <josephonyekachi77@gmail.com>
2023-04-19 21:15:12 -0800

    updated

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -65,64 +65,7 @@
 
 
     <!-- Investor Section Starts Here -->
-    <section class="investor-section padding-bottom">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-6 col-md-10">
-                    <div class="section__header text-center">
-                        <h2 class="section__header-title">Our Best Investor</h2>
-                        <p>Aenean vulputate eleifend tellus. Aenean leo ligul porttitoeu consequat vitae eleifend.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row justify-content-center gy-5">
-                <div class="col-xl-3 col-lg-4 col-sm-6">
-                    <div class="investor__item">
-                        <div class="investor__item-thumb">
-                            <img src="assets/images/investor/investor1.png" alt="investor">
-                        </div>
-                        <div class="investor__item-content">
-                            <h3 class="name">Robart Williams</h3>
-                            <p class="invest-amount">Invest 250 USD</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-sm-6">
-                    <div class="investor__item">
-                        <div class="investor__item-thumb">
-                            <img src="assets/images/investor/investor2.png" alt="investor">
-                        </div>
-                        <div class="investor__item-content">
-                            <h3 class="name">Munna Ahmed</h3>
-                            <p class="invest-amount">Invest 350 USD</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-sm-6">
-                    <div class="investor__item">
-                        <div class="investor__item-thumb">
-                            <img src="assets/images/investor/investor3.png" alt="investor">
-                        </div>
-                        <div class="investor__item-content">
-                            <h3 class="name">Maliha Mahtab</h3>
-                            <p class="invest-amount">Invest 450 USD</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-sm-6">
-                    <div class="investor__item">
-                        <div class="investor__item-thumb">
-                            <img src="assets/images/investor/investor4.png" alt="investor">
-                        </div>
-                        <div class="investor__item-content">
-                            <h3 class="name">Taposhi Ahmed</h3>
-                            <p class="invest-amount">Invest 550 USD</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('frontend.include.investors')
     <!-- Investor Section Ends Here -->
 
 
