82adaa59b73dd46fd1d15af8246fd7ef8bf3f464
Myron <josephonyekachi77@gmail.com>
2023-04-19 20:33:25 -0800

    updated

---
+++ resources/views/frontend/include/header.blade.php
@@ -0,0 +1,129 @@
+<div class="preloader">
+    <div class="preinnner">
+        <div class="ring"></div>
+        <div class="ring"></div>
+        <div class="ring"></div>
+    </div>
+</div>
+<div class="overlay"></div>
+
+<!-- Coin widget Section Starts Here -->
+<div style=" background-color: #00001c; padding-bottom:2px;"><iframe src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=dark&pref_coin_id=1505&invert_hover=" width="100%" height="36px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;"></iframe></div>
+<!-- Coin widget Section Ends Here -->
+
+<!-- Header Section Starts Here -->
+<div class="header">
+    <div class="header-top">
+        <div class="container">
+            <div class="header__top__wrapper d-flex flex-wrap align-items-center justify-content-center justify-content-md-between text-center">
+                <ul class="contacts d-flex flex-wrap justify-content-center">
+                    <li><a href="https://template.viserlab.com/cdn-cgi/l/email-protection#f793929a98b7909a969e9bd994989a"><i class="las la-envelope-open"></i> <span class="__cf_email__" data-cfemail="7216171f1d32151f131b1e5c111d1f">[email&#160;protected]</span></a></li>
+                    <li><a href="tel:129075"><i class="las la-phone-volume"></i> 123 - 456 - 7890</a></li>
+                </ul>
+                <div class="right__area d-flex flex-wrap align-items-center mt-3 mt-md-0">
+                    <select name="language" class="nice-select custom--scrollbar">
+                        <option>English</option>
+                        <option>Bangla</option>
+                    </select>
+                    <a href="dashboard.html" class="user__thumb ms-3 me-3 me-lg-0">
+                        <img src="assets/images/dashboard/user.png" alt="dashboard">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="header-bottom-area">
+                <div class="logo"><a href="index.html"><img src="assets/images/logo.png" alt="logo"></a></div>
+                <ul class="menu">
+                    <li>
+                        <a href="#0">Home</a>
+                        <ul class="sub-menu">
+                            <li><a href="index.html">Home One</a></li>
+                            <li><a href="index2.html">Home Two</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="plan.html">Plan</a>
+                    </li>
+                    <li>
+                        <a href="#0">Pages</a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="dashboard.html">User Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="investor.html">Investor</a>
+                            </li>
+                            <li>
+                                <a href="faq.html">FAQs</a>
+                            </li>
+                            <li>
+                                <a href="policy.html">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="error-page.html">404</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#0">Blog</a>
+                        <ul class="sub-menu">
+                            <li><a href="blog-grid.html">Blog Grid</a></li>
+                            <li><a href="blog-list.html">Blog List</a></li>
+                            <li><a href="blog-details.html">Blog Details</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+
+                    <li class="d-none d-lg-block">
+                        <a href="#0" class="search--btn"><i class="fas fa-search"></i></a>
+                    </li>
+
+                    <li class="p-0 d-lg-none mt-3 mt-lg-0">
+                        <div class="button__wrapper">
+                            <a href="sign-up.html" class="cmn--btn">Register</a>
+                            <a href="sign-in.html" class="cmn--btn">Login</a>
+                        </div>
+                    </li>
+                </ul> <!-- Menu End -->
+
+                <div class="button__wrapper d-none d-lg-block">
+                    <a href="sign-up.html" class="cmn--btn">Register</a>
+                    <a href="sign-in.html" class="cmn--btn">Login</a>
+                </div>
+
+
+                <div class="header-trigger-wrapper d-flex d-lg-none align-items-center">
+                    <div class="mobile-nav-right d-flex align-items-center"></div>
+                    <a href="#0" class="search--btn me-4 text--base"><i class="fas fa-search"></i></a>
+                    <div class="header-trigger d-block d--none">
+                        <span></span>
+                    </div>
+                </div> <!-- Trigger End-->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Header Section Ends Here -->
+
+
+<!-- Search Form Starts Here -->
+<div class="search__form__wrapper">
+    <div class="form__inner">
+        <form class="search__form">
+            <div class="form-group">
+                <input type="text" class="form-control form--control" placeholder="Search Here...">
+                <button type="submit" class="cmn--btn btn">Search</button>
+            </div>
+        </form>
+        <button class="btn-close btn-close-white"></button>
+    </div>
+</div>
+<!-- Search Form Ends Here -->
