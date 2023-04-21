e67ea792595bcc7bba0acd812c909b36033aee22
Myron <josephonyekachi77@gmail.com>
2023-04-20 23:23:27 -0800

    updated

---
+++ resources/views/frontend/include/faq.blade.php
@@ -0,0 +1,50 @@
+<section class="faq-section padding-top padding-bottom bg_img" style="background: url(assets/images/faq/bg.png);">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-7 col-md-10">
+                <div class="section__header text-center max-p">
+                    <h2 class="section__header-title">Frequently Asked Questions</h2>
+                    <p>Aenean vulputate eleifend tellus. Aenean leo ligul porttitoeu consequat vitae eleifend.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-8 col-lg-10">
+                <div class="faq__wrapper">
+                    <div class="faq__item">
+                        <div class="faq__item-title">
+                            <h4 class="title">Why You should become an Investor?</h4>
+                        </div>
+                        <div class="faq__item-content">
+                            <p>Fringilla mauris amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales augue velit cursus nunc quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy idmetus</p>
+                        </div>
+                    </div>
+                    <div class="faq__item">
+                        <div class="faq__item-title">
+                            <h4 class="title">Why You Choose Us?</h4>
+                        </div>
+                        <div class="faq__item-content">
+                            <p>Fringilla mauris amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales augue velit cursus nunc quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy idmetus</p>
+                        </div>
+                    </div>
+                    <div class="faq__item open active">
+                        <div class="faq__item-title">
+                            <h4 class="title">What is Hyip Investment?</h4>
+                        </div>
+                        <div class="faq__item-content">
+                            <p>Fringilla mauris amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales augue velit cursus nunc quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy idmetus</p>
+                        </div>
+                    </div>
+                    <div class="faq__item">
+                        <div class="faq__item-title">
+                            <h4 class="title">How to do we work?</h4>
+                        </div>
+                        <div class="faq__item-content">
+                            <p>Fringilla mauris amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales augue velit cursus nunc quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy idmetus</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
