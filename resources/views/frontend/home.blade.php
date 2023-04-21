4d78fb553adc07df6241558964446693b885ea86
Myron <josephonyekachi77@gmail.com>
2023-04-20 23:22:01 -0800

    updated

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -70,74 +70,7 @@
 
 
     <!-- Download Section Starts Here -->
-    <section class="download-section padding-bottom section-bg-two overflow-hidden">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-7">
-                    <div class="section__header">
-                        <h2 class="section__header-title">Download Our App</h2>
-                        <p>Curabitur ullamcorper ultricies nisi Tupural Engle Nam eget dui. Etiam rhoncus.</p>
-                    </div>
-                    <div class="button__wrapper">
-                        <a href="sign-up.html" class="cmn--btn download-btn">
-                            <div class="icon"><i class="fab fa-google-play"></i></div>
-                            <div class="text">
-                                <p>DOWNLOAD</p>
-                                <p class="for">For Android</p>
-                            </div>
-                        </a>
-                        <a href="sign-up.html" class="cmn--btn download-btn">
-                            <div class="icon"><i class="fab fa-apple"></i></div>
-                            <div class="text">
-                                <p>DOWNLOAD</p>
-                                <p class="for">For IOS</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="counter__wrapper row gy-4 gy-sm-5 pt-4 pt-sm-5">
-                        <div class="col-6 col-sm-6 col-md-4 col-lg-6 col-xl-4">
-                            <div class="counter__item">
-                                <div class="counter__item-header d-flex align-items-center">
-                                    <div class="icon">
-                                        <img src="assets/images/download/icon1.png" alt="counter">
-                                    </div>
-                                    <h2 class="title"><span class="odometer" data-odometer-final="39">0</span>M</h2>
-                                </div>
-                                <p class="info">Active Members</p>
-                            </div>
-                        </div>
-                        <div class="col-6 col-sm-6 col-md-4 col-lg-6 col-xl-4">
-                            <div class="counter__item">
-                                <div class="counter__item-header d-flex align-items-center">
-                                    <div class="icon">
-                                        <img src="assets/images/download/icon2.png" alt="counter">
-                                    </div>
-                                    <h2 class="title"><span class="odometer" data-odometer-final="150">0</span>M</h2>
-                                </div>
-                                <p class="info">Our Total Deposit</p>
-                            </div>
-                        </div>
-                        <div class="col-6 col-sm-6 col-md-4 col-lg-6 col-xl-4">
-                            <div class="counter__item">
-                                <div class="counter__item-header d-flex align-items-center">
-                                    <div class="icon">
-                                        <img src="assets/images/download/icon3.png" alt="counter">
-                                    </div>
-                                    <h2 class="title"><span class="odometer" data-odometer-final="40">0</span>M</h2>
-                                </div>
-                                <p class="info">Our Total Withdraw</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5 d-none d-lg-block">
-                    <div class="section__thumb profit__calculation__thumb ">
-                        <img src="assets/images/download/thumb.png" alt="download">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('frontend.include.download')
     <!-- Download Section Ends Here -->
 
 
