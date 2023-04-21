ed17667bee9e7d064ea90bb899c04e156e73d300
Myron <josephonyekachi77@gmail.com>
2023-04-20 23:26:26 -0800

    updated

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -85,46 +85,7 @@
 
 
     <!-- Testimonial Section Starts Here -->
-    <section class="testimonial-section padding-top padding-bottom">
-        <div class="container">
-            <div class="row gy-4">
-                <div class="col-lg-5">
-                    <div class="testimonial__img__slider">
-                        <div class="single-slide">
-                            <div class="testimonial__thumb">
-                                <img src="assets/images/testimonial/item1.png" alt="testimonial">
-                            </div>
-                        </div>
-                        <div class="single-slide">
-                            <div class="testimonial__thumb">
-                                <img src="assets/images/testimonial/item2.png" alt="testimonial">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-7">
-                    <div class="testimonial__content__slider">
-                        <div class="single-slide">
-                            <div class="testimonial__content">
-                                <div class="icon"><i class="fas fa-quote-left"></i></div>
-                                <p>Maecenas tempus tellus egcondi entuhoncus seuam semper libero sit amet adipiscing sque sesum.amuam nunc, blandit vel, luctus pvinar hendrerilorem. aecenas nec odio</p>
-                                <h3 class="name">Rasel Pranto</h3>
-                                <span class="designation text-white">Front-end Web Developer</span>
-                            </div>
-                        </div>
-                        <div class="single-slide">
-                            <div class="testimonial__content">
-                                <div class="icon"><i class="fas fa-quote-left"></i></div>
-                                <p>Maecenas tempus tellus egcondi entuhoncus seuam semper libero sit amet adipiscing sque sesum.amuam nunc, blandit vel, luctus pvinar hendrerilorem. aecenas nec odio</p>
-                                <h3 class="name">Munna Ahmed</h3>
-                                <span class="designation text-white">Front-end Web Developer</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('frontend.include.test')
     <!-- Testimonial Section Ends Here -->
 
 
