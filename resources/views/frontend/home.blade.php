b04d7f1f349f8fc8a6740089ad4fe1defa82d2a9
Myron <josephonyekachi77@gmail.com>
2023-04-20 23:32:18 -0800

    updated

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -95,37 +95,7 @@
 
 
     <!-- Brand Section Starts Here -->
-    <div class="padding-top padding-bottom">
-        <div class="container">
-            <div class="brand__slider">
-                <div class="single-slide">
-                    <div class="brand__item">
-                        <img src="assets/images/brand/item1.png" alt="brand">
-                    </div>
-                </div>
-                <div class="single-slide">
-                    <div class="brand__item">
-                        <img src="assets/images/brand/item1.png" alt="brand">
-                    </div>
-                </div>
-                <div class="single-slide">
-                    <div class="brand__item">
-                        <img src="assets/images/brand/item1.png" alt="brand">
-                    </div>
-                </div>
-                <div class="single-slide">
-                    <div class="brand__item">
-                        <img src="assets/images/brand/item1.png" alt="brand">
-                    </div>
-                </div>
-                <div class="single-slide">
-                    <div class="brand__item">
-                        <img src="assets/images/brand/item1.png" alt="brand">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('frontend.include.brand')
     <!-- Brand Section Ends Here -->
 
 
---
+++ resources/views/frontend/include/brand.blade.php
@@ -0,0 +1,31 @@
+<div class="padding-top padding-bottom">
+    <div class="container">
+        <div class="brand__slider">
+            <div class="single-slide">
+                <div class="brand__item">
+                    <img src="assets/images/brand/item1.png" alt="brand">
+                </div>
+            </div>
+            <div class="single-slide">
+                <div class="brand__item">
+                    <img src="assets/images/brand/item1.png" alt="brand">
+                </div>
+            </div>
+            <div class="single-slide">
+                <div class="brand__item">
+                    <img src="assets/images/brand/item1.png" alt="brand">
+                </div>
+            </div>
+            <div class="single-slide">
+                <div class="brand__item">
+                    <img src="assets/images/brand/item1.png" alt="brand">
+                </div>
+            </div>
+            <div class="single-slide">
+                <div class="brand__item">
+                    <img src="assets/images/brand/item1.png" alt="brand">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
