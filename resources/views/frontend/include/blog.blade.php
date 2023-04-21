9c97d4e3e906b1ff1bd0fed2504cff051ba85557
Myron <josephonyekachi77@gmail.com>
2023-04-20 23:30:08 -0800

    updated

---
+++ resources/views/frontend/include/blog.blade.php
@@ -0,0 +1,74 @@
+<section class="blog-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-7 col-md-10">
+                <div class="section__header max-p text-center">
+                    <h2 class="section__header-title">See Our Latest Blog & Tips</h2>
+                    <p>Aenean vulputate eleifend tellus. Aenean leo ligul porttitoeu consequat vitae eleifend.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center g-4">
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="post__item">
+                    <div class="post__item-thumb">
+                        <img src="assets/images/blog/item1.png" alt="blog">
+                        <div class="thumb__content">
+                            <h3 class="title"><a href="blog-details.html">Aenean Ligul Porttitoe Consequat Ditae</a></h3>
+                            <ul class="post-meta d-flex flex-wrap m-0 justify-content-between">
+                                <li>
+                                    <i class="fas fa-user"></i>
+                                    Thomas Addican
+                                </li>
+                                <li>
+                                    <i class="fas fa-calendar"></i>
+                                    15 Aug 2020
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="post__item">
+                    <div class="post__item-thumb">
+                        <img src="assets/images/blog/item2.png" alt="blog">
+                        <div class="thumb__content">
+                            <h3 class="title"><a href="blog-details.html">Aenean Ligul Porttitoe Consequat Ditae</a></h3>
+                            <ul class="post-meta d-flex flex-wrap m-0 justify-content-between">
+                                <li>
+                                    <i class="fas fa-user"></i>
+                                    Thomas Addican
+                                </li>
+                                <li>
+                                    <i class="fas fa-calendar"></i>
+                                    15 Aug 2020
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="post__item">
+                    <div class="post__item-thumb">
+                        <img src="assets/images/blog/item3.png" alt="blog">
+                        <div class="thumb__content">
+                            <h3 class="title"><a href="blog-details.html">Aenean Ligul Porttitoe Consequat Ditae</a></h3>
+                            <ul class="post-meta d-flex flex-wrap m-0 justify-content-between">
+                                <li>
+                                    <i class="fas fa-user"></i>
+                                    Thomas Addican
+                                </li>
+                                <li>
+                                    <i class="fas fa-calendar"></i>
+                                    15 Aug 2020
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
