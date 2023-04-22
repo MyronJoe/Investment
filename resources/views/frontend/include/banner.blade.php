d4ffa79213c2be59d849c8335e097908c5fa42d9
Myron <josephonyekachi77@gmail.com>
2023-04-21 23:17:04 -0800

    updated

--- resources/views/frontend/include/banner.blade.php
+++ resources/views/frontend/include/banner.blade.php
@@ -1,11 +1,13 @@
 <section class="banner-section overflow-hidden">
     <div class="container">
         <div class="banner__wrapper d-flex align-items-center justify-content-between">
+            
             <div class="banner__content">
                 <h1 class="title">Best Hyip Investment Platform</h1>
                 <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper libero hyip investment pandding</p>
                 <a href="sign-in.html" class="cmn--btn">Get Started</a>
             </div>
+
             <div class="banner__thumb d-none d-lg-block">
                 <img src="assets/images/banner/thumb.png" alt="banner">
                 <div class="shapes">
@@ -15,6 +17,7 @@
                     <img src="assets/images/banner/sm-coin.png" alt="banner" class="shape shape4">
                 </div>
             </div>
+
         </div>
     </div>
 </section>
