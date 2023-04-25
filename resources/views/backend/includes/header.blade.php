2c18a8ff4b8977d62007632250305213f95e9f4b
Myron <josephonyekachi77@gmail.com>
2023-04-24 20:57:42 -0800

    updated

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -17,9 +17,7 @@
                             <a class="dropdown-toggle" data-toggle="dropdown"><img class="img-responsive rounded-circle" src="backend/images/layout_img/user_img.jpg" alt="#" /><span class="name_user">{{ Auth::user()->name }}</span></a>
                             <div class="dropdown-menu">
                                 <a class="dropdown-item" href="profile.html">My Profile</a>
-                                <a class="dropdown-item" href="settings.html">Settings</a>
-                                <a class="dropdown-item" href="help.html">Help</a>
-                                <a class="dropdown-item" href="#"><span>Log Out</span> <i class="fa fa-sign-out"></i></a>
+                                <a class="dropdown-item" href="{{route('logout')}}"><span>Log Out</span> <i class="fa fa-sign-out"></i></a>
                             </div>
                         </li>
                     </ul>
