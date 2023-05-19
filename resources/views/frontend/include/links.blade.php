4e3b18b3109e8103255b1648b2bb5182c7ad4c56
Myron <josephonyekachi77@gmail.com>
2023-05-19 02:20:05 -0800

    updated now

--- resources/views/frontend/include/links.blade.php
+++ resources/views/frontend/include/links.blade.php
@@ -5,15 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <!-- BootStrap Link -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/animate.css">
-
     <!-- Icon Link -->
     <link rel="stylesheet" href="assets/css/all.min.css">
     <link rel="stylesheet" href="assets/css/line-awesome.min.css">
-
     <!-- Plugings Link -->
     <link rel="stylesheet" href="assets/css/slick.css">
     <link rel="stylesheet" href="assets/css/nice-select.css">
